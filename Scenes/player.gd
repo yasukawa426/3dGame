79ebae7c3c307cbc5c39a32f658875631768ca5c
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-02-21 23:51:12 -0300

    Testes de animação e modelos
    Testes que eu fiz para aprender como importar modelos e animações para o godot. Modelo inicial baixado de sketchfab e depois importado no mixamo para refazer o esqueleto e baixar as animações configuradas.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -12,6 +12,8 @@ const JUMP_VELOCITY:float = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+	
+
 
 func _physics_process(delta:float):
 	#Chama a função de movimento
@@ -39,12 +41,20 @@ func _handle_movement(delta:float):
 	var input_dir = Input.get_vector("move_left", "move_right", "move_foward", "move_back")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
+		$shrek_model/AnimationPlayer.play("walk")
+		
 		velocity.x = direction.x * current_speed
 		velocity.z = direction.z * current_speed
 	#Caso não estiver com nenhum botão apertado, desacelera até 0.
 	else:
 		velocity.x = move_toward(velocity.x, 0, current_speed)
 		velocity.z = move_toward(velocity.z, 0, current_speed)
+		
+		if Input.is_action_just_pressed("dance"):
+			$shrek_model/AnimationPlayer.play("dance")
+			
+		elif not $shrek_model/AnimationPlayer.is_playing():
+			$shrek_model/AnimationPlayer.play("idle")
 	
 	#Move com os vetores criados
 	move_and_slide()
