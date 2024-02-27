54a0c15fecb90014a2a7e058e7b245e4b2ce3535
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-02-27 15:23:05 -0300

    Adicionado informação de teste dizendo qual objeto o usuário está olhando.

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -38,10 +38,19 @@ func _debug_inputs(event: InputEvent):
 func _update_test_hud():
 	var stamina: float = $SubViewportContainer/SubViewport/TestStage/Player.current_stamina
 	var sprinting: bool = $SubViewportContainer/SubViewport/TestStage/Player.is_sprinting
+	var colliding_object = $SubViewportContainer/SubViewport/TestStage/Player/CameraPivot/InteractRay.colliding_object
+	var colliding_object_string: String = ""
+	
+	if colliding_object != null:
+		colliding_object_string = str(colliding_object)
+	else:
+		colliding_object_string = "None"
+	
 	
 	var test_string: String = ""
 	
 	test_string += "Stamina: " + str(stamina) + "\n"
 	test_string += "Sprinting: " + str(sprinting) + "\n"
+	test_string += "Ray Colliding Object: " + colliding_object_string + "\n"
 	
 	$TestHUD.update_test_info(test_string)
