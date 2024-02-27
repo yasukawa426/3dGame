0e69f3e149d63cab062b16790bf96dbce1c8ec59
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@outlook.com>
2024-02-27 10:00:39 -0300

    Código refatorado

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody3D
 
+#Configuração base do jogador
 #Velocidade base do jogador
 const BASE_SPEED:float = 5.0
-
+#Em quanto correr aumenta a velocidade
+const SPRINT_MULTIPLIER: float = 1.5
+#Velocidade de pulo
+const JUMP_VELOCITY:float = 4.5
 #Sensibilidade do mouse
 const SENSITIVITY:float = 1
 
+
 #Velocidade atual do jogador, alterada em algumas situações.
 var current_speed:float = BASE_SPEED
-
-#Velocidade de pulo
-const JUMP_VELOCITY:float = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -29,7 +31,7 @@ func _ready():
 	#Capturamos o mouse para futuramente usalo como movimento da camera
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
+#Chamado a cada frame de fisica
 func _physics_process(delta:float):
 	#Chama a função de movimento
 	_handle_movement(delta)
@@ -45,36 +47,17 @@ func _input(event):
 func _unhandled_input(event:InputEvent):
 	#Caso o evento for do tipo movimento do mouse
 	if event is InputEventMouseMotion:
-		#separamos a rotação para evitar rotar o mundo
-		head.rotate_y(-event.relative.x * SENSITIVITY/100)
-		camera.rotate_x(-event.relative.y * SENSITIVITY/100)
+		_handle_mouse_motion(event)
 		
-		#limitamos quanto o jogador consegue movimentar a camera no eixo x
-		#evitando girar completamente
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+	#Caso for algum evento de tecla
+	elif event is InputEventKey:
+		_handle_keyboard_input(event)
 		
-	#Caso for algum evento de ação
-	elif event is InputEvent:
-		
-		#Checa se o botão de correr está segurado e então altera a current_speed
-		if Input.is_action_pressed("sprint"):
-			current_speed = BASE_SPEED * 1.5
-		else:
-			current_speed = BASE_SPEED
-			
-		# Handle jump.
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
-			
-		# Pega a direção do movimento baseado nos inputs (esquerda, direita, frente e traz)
-		_input_dir = Input.get_vector("move_left", "move_right", "move_foward", "move_back")
-	
 	else:
 		pass
 
 #Função responsável pela movimentação do player
 func _handle_movement(delta:float):
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -97,5 +80,27 @@ func _handle_movement(delta:float):
 	#Move com os vetores criados
 	move_and_slide()
 
+#Função responsável por lidar com o movimento do mouse
+func _handle_mouse_motion(event: InputEventMouseMotion):
+	#separamos a rotação para evitar rotar o mundo
+	head.rotate_y(-event.relative.x * SENSITIVITY/100)
+	camera.rotate_x(-event.relative.y * SENSITIVITY/100)
+		
+	#limitamos quanto o jogador consegue movimentar a camera no eixo x
+	#evitando girar completamente
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))	
 
-
+#Fumção responsável por lidar com o teclado
+func _handle_keyboard_input(event: InputEventKey):
+	#Checa se o botão de correr está segurado e então altera a current_speed
+	if Input.is_action_pressed("sprint"):
+		current_speed = BASE_SPEED * SPRINT_MULTIPLIER
+	else:
+		current_speed = BASE_SPEED
+		
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	# Pega a direção do movimento baseado nos inputs (esquerda, direita, frente e traz)
+	_input_dir = Input.get_vector("move_left", "move_right", "move_foward", "move_back")
